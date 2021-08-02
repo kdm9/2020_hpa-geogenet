096b3e4ded8e0cf88ae26ecb509f4480244d30f3
Kevin Murray <foss@kdmurray.id.au>
2021-08-02 12:30:13 +1000

    Construct plots done

--- 02_construct-plots.R
+++ 02_construct-plots.R
@@ -1,6 +1,7 @@
-install.packages(c("ggspatial", "ggplot2", "sf", "rnaturalearth",
-                   "rnaturalearthdata", "ggforce"))
+# # Construct-related plots for the paper
 
+#install.packages(c("ggspatial", "ggplot2", "sf", "rnaturalearth",
+#                   "rnaturalearthdata", "ggforce"))
 library(tidyverse)
 library(ggspatial)
 library(ggplot2)
@@ -14,11 +15,15 @@ theme_set(theme_bw())
 meta = read_tsv("data/metadata/europe-metadata-geoclust.tsv")
 load("data/cache/02_construct_admix_prop.Rds", verbose=T)
 
-# # Pie map plots
-
-plt  = crossval_admix_prop %>%
+csxvap = crossval_admix_prop %>%
     select(mdl, K, rep, both) %>%
     unnest(both) %>%
+    mutate(geo.clust = sprintf("GC%02d", site))
+
+
+# # Pie map plots
+
+plt  = csxvap %>%
     mutate(group = dist(cbind(longitude, latitude)) %>%
                        hclust() %>%
                        cutree(h=0.8)) %>%
@@ -48,7 +53,14 @@ ggplot(eu) +
     coord_sf(xlim = eu.bbox[1:2], ylim = eu.bbox[c(4,3)], expand = FALSE) +
     labs(x=NULL, y=NULL) +
     theme(legend.position="none", panel.grid=element_blank())
-ggsave("out/Hpa_cs_v2/Hpa_cs_k2-piemap.png", height=6.3, width=7, unit="in", dpi=1200)
+ggsave("out/plot/02_Hpa_cs_k2-piemap.svg", height=6.3, width=7, unit="in", dpi=1200)
+
+ggplot() +
+    plt %>%
+       filter(mdl=="sp", K==2, rep==1) %>%
+       pull(plot) +
+    coord_fixed()
+ggsave("out/plot/02_Hpa_cs_k2-justpies.svg", height=6.3, width=7, unit="in", dpi=1200)
 
 ggplot(eu) +
     geom_sf(fill="white", colour="grey") +
@@ -60,15 +72,23 @@ ggplot(eu) +
     coord_sf(xlim = eu.bbox[1:2], ylim = eu.bbox[c(4,3)], expand = FALSE) +
     labs(x=NULL, y=NULL) +
     theme(legend.position="none", panel.grid=element_blank())
-ggsave("out/Hpa_cs_v2/Hpa_cs_k3-piemap.png", height=6.3, width=7, unit="in", dpi=1200)
+ggsave("out/plot/02_Hpa_cs_k3-piemap.svg", height=6.3, width=7, unit="in", dpi=1200)
+
+ggplot() +
+    plt %>%
+       filter(mdl=="sp", K==3, rep==1) %>%
+       pull(plot) +
+    coord_fixed()
+ggsave("out/plot/02_Hpa_cs_k3-justpies.svg", height=6.3, width=7, unit="in", dpi=1200)
 
 # # Structure bar plots
 
-k23_cs = crossval_admix_prop %>%
+samp.within.eur.geno.ok = readLines("data/metadata/samples_within_europe_genotype_ok.txt")
+
+k23_cs = csxvap %>%
     filter(K %in% 2:3, rep==1) 
 
 
-samp.within.eur.geno.ok = readLines("data/metadata/samples_within_europe_genotype_ok.txt")
 admix.indiv = readLines("data/admixture/individualOrdered.txt")
 
 k23_admix = foreach(K=2:3, rep=1:10, .combine=rbind) %do%
@@ -89,15 +109,25 @@ k23_admix_pop = k23_admix %>%
     inner_join(admix.meta, by=c("indiv"="ind")) %>%
     group_by(pop, K, rep, geo.clust) %>%
     summarise_at(vars(admix.prop, latitude, longitude), mean) %>%
-    mutate(set=sprintf("ADMIXTURE K%d", K))
+    mutate(set=sprintf("AM K%d", K))
 
 k23_cs_pre = k23_cs %>%
-    select(-size, -longitude, -latitude) %>%
-    mutate(set=sprintf("conStruct K%d %s", K, c("sp"="Spatial", "nsp"="Non-spatial")[mdl]), pop=as.integer(pop)) %>%
+    mutate(set=sprintf("CS K%d %s", K, mdl), pop=as.integer(pop)) %>%
     rename(admix.prop=proportion)
 
 
-barplot.dat = bind_rows(k23_cs_pre, k23_admix_pop)
+barplot.dat = bind_rows(k23_cs_pre, k23_admix_pop) %>%
+    select(set, K, rep, geo.clust, pop, admix.prop) %>%
+    mutate(pop=as.factor(pop))
 
-kview(k23_cs)
-kview(barplot.dat)
+ggplot(barplot.dat, aes(x=geo.clust, y=admix.prop)) +
+    geom_bar(aes(colour=pop, fill=pop), stat="identity", position="stack") +
+    facet_grid(.~set) +
+    scale_colour_brewer(palette="Paired") +
+    scale_fill_brewer(palette="Paired") +
+    coord_flip() +
+    theme_classic() +
+    labs(x=NULL, y=NULL) +
+    theme(legend.position="none", axis.text=element_blank(), axis.ticks=element_blank(),
+        strip.text.x=element_text(angle=90))
+ggsave("out/plot/02_construct-admixture-barplots.svg", width=3, height=6)
